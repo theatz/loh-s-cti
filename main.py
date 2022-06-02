1d7de4b6e3b5f64adc8e2b359cd1ca7de31deec1
theatz <36051963+theatz@users.noreply.github.com>
2022-06-02 04:11:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import telebot
 import os
 
-bot = telebot.TeleBot(os.environ['TOKEN'])
+token = os.environ['TOKEN']
 chatID = os.environ['CHAT']
+print(token, chatID)
+bot = telebot.TeleBot(token)
+
 
 @bot.message_handler()
 def repeat_all_messages(message): # Название функции не играет никакой роли
+    print(message.message_id)
     bot.forward_message(chatID, message.chat.id, message.message_id)
 
 if __name__ == '__main__':
